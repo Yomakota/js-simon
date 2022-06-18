f8d99e9f2d19994ab6f9799b5e509a6c7002408e
Yomakota <teoperitore@outlook.com>
2022-06-18 14:21:26 +0200

    add corrected function with while

--- js/script.js
+++ js/script.js
@@ -32,25 +32,97 @@ alert(randomNum);
 const message = document.getElementById('message'); 
 
 // timing function per impostare un timer
-setTimeout(function () {
-    
+// setTimeout(function () {
+
+//     const userNum = []; //array dove pushare i numeri 'azzeccati' dall'utente
+//     console.log(userNum);
+
+//     // ciclo for per creare 5 prompt
+//     for (let i = 0; i < randomNum.length; i++) {
+
+//         // prompt per chiedere i numeri all'utente
+//         const insertNum = parseInt(prompt('Quale ricordi?'));
+
+//         // condizione per controllare se i numeri inseriti sono uguali a quelli generati dal programma 
+//         if (randomNum.includes(insertNum)) {
+//         // e se lo sono pusharli nell array per fare un confronto
+//             userNum.push(insertNum);
+//         }
+
+//         // stampo il punteggio
+//         message.innerHTML = `Hai indovinato ${userNum.length} numero/i: ${userNum} `;
+
+//     }
+// }, 30000);
+
+//-------------------------------------
+//Function corretta con ciclo For
+//-------------------------------------
+
+
+// funzione corretta per non vincere con lo stesso numero
+// setTimeout(() => {
+
+//     const userNum = []; //array dove pushare i numeri 'azzeccati' dall'utente
+//     console.log(userNum);
+
+//     // ciclo for per creare 5 prompt
+//     for (let i = 0; i < randomNum.length; i++) {
+
+//         // prompt per chiedere i numeri all'utente
+//         const insertNum = parseInt(prompt('Quale ricordi?'));
+
+//         // condizione per controllare se i numeri inseriti sono uguali a quelli generati dal programma 
+//         if (randomNum.includes(insertNum) && !userNum.includes(insertNum)) { // aggiungo condizione per non ripetere lo stesso num inserito prima dall'utente
+
+//             // e se lo sono pusharli nell array per fare un confronto
+//             userNum.push(insertNum);
+//         }
+
+//         // stampo il punteggio
+//         message.innerHTML = `Hai indovinato ${userNum.length} numero/i: ${userNum} `;
+
+//     }
+// }, 30000);
+
+//-------------------------------------
+//Function corretta con ciclo While
+//-------------------------------------
+
+setTimeout(() => {
+
     const userNum = []; //array dove pushare i numeri 'azzeccati' dall'utente
     console.log(userNum);
 
     // ciclo for per creare 5 prompt
-    for (let i = 0; i < randomNum.length; i++) {
-    
+    while (userNum.length < 5) {
+
         // prompt per chiedere i numeri all'utente
         const insertNum = parseInt(prompt('Quale ricordi?'));
 
-        // condizione per controllare se i numeri inseriti sono uguali a quelli generati dal programma 
-        if (randomNum.includes(insertNum)) {
-        // e se lo sono pusharli nell array per fare un confronto
+        // aggiungo il numero solo se non compreso nel array coi numeri dell'utente
+        if (!userNum.includes(insertNum)) { // 
             userNum.push(insertNum);
         }
 
-        // stampo il punteggio
-        message.innerHTML = `Hai indovinato ${userNum.length} numero/i: ${userNum} `;
-
+        // // stampo il punteggio
+        // message.innerHTML = `Hai indovinato ${userNum.length} numero/i: ${userNum} `;
     }
-}, 30000)
+
+    // Ora ho ottenuto dentro userNum i 5 numeri univoci forniti dall'utente
+    // Confronto gli array per capire quali sono i numeri indovinati dall'utente e li salvo 
+    // in un terzo Array
+
+    const mindedNum = [];
+    for (let i = 0; i < userNum.length; i++) {
+
+        const singleUserNum = userNum[i];
+
+        if (randomNum.includes(singleUserNum)) {
+            mindedNum.push(singleUserNum);
+        }
+
+        // // stampo il punteggio
+        message.innerHTML = `Hai indovinato ${mindedNum.length} numero/i: ${mindedNum.join(' - ')} `;
+    }
+}, 30000);
